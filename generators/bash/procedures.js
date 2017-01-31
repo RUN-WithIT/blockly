96c860616eb2f5059935c08b6371e51eb4b45eac
Grant Hunter <gr@nt-hunter.ca>
2017-01-30 18:32:48 -0700

    Added first cut of predefined function calls

--- generators/bash/procedures.js
+++ generators/bash/procedures.js
@@ -84,3 +84,15 @@ Blockly.bash['procedures_ifreturn'] = function(block) {
   code += 'fi\n';
   return code;
 };
+
+
+Blockly.bash['predefined_procedures'] = function(block) {
+  var funcName = block.getFieldValue('NAME');
+  var args = [];
+  for (var i = 0; i < block.itemCount_; i++) {
+      args.push(Blockly.bash.valueToCode(block, 'ARG' + i,
+          Blockly.bash.ORDER_COMMA) || '');
+    }
+  var code = funcName + ' ' +  args.join(' ') ;
+  return ['`' + code + '`', Blockly.bash.ORDER_FUNCTION_CALL];
+};
